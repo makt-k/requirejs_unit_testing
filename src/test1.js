c056489e55d4e8c79dd27a5062394d2ec93b0cbb
unknown <kmak@KMAK-PC.artfact.local>
2014-06-05 18:48:03 +0100

    added test suite

---
+++ src/test1.js
@@ -0,0 +1,26 @@
+
+define(['underscore', 'backbone', 'mocha', 'chai'], function(_, backbone, mocha, chai) {
+	describe("BDD example", function () {
+    // Runs once before all tests start.
+    before(function () {
+      // Add a local function.
+      this.hello = function () {
+        return "Hello world!";
+      };
+    });
+
+    // Runs once when all tests finish.
+    after(function () {
+      // Remove local function.
+      this.hello = null;
+    });
+
+    it("should return expected string result", function () {
+      // Chai BDD-style assertion.
+      expect(this.hello()).to
+        .be.a("string").and
+        .equal("Hello world!");
+    });
+  });
+
+});
